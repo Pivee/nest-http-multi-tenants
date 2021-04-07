11f6bed4cf031b9228122875491d070f32c1e207
Pivithuru Jayakody <piveedj@gmail.com>
2021-04-07 18:00:35 +0530

    Refactor TENANT_CONNECTION to fit HTTP and RPC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './modules/common/common.module';
 import { TenantSpecificModule } from './modules/tenant-specific/tenant-specific.module';
-import { TenancyMiddleware } from './tenancy/middleware/tenancy.middleware';
 import { TenancyModule } from './tenancy/tenancy.module';
 import { TenantsService } from './tenancy/tenants/tenants.service';
 
@@ -17,11 +11,4 @@ import { TenantsService } from './tenancy/tenants/tenants.service';
   controllers: [AppController],
   providers: [AppService, TenantsService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(TenancyMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
--- src/tenancy/connection/tenant-connection/tenant-connection.module.ts
+++ src/tenancy/connection/tenant-connection/tenant-connection.module.ts
@@ -1,18 +1,20 @@
 import { FactoryProvider, Global, Module, Scope } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
+import { TenantsModule } from 'src/tenancy/tenants/tenants.module';
+import { TenantsService } from 'src/tenancy/tenants/tenants.service';
 import { createConnection, getConnectionManager } from 'typeorm';
-import { IRequestWithTenantConnection } from '../../interfaces/request-with-tenant-connection.interface';
 
 const connectionFactory: FactoryProvider = {
   provide: 'TENANT_CONNECTION',
   scope: Scope.REQUEST,
-  useFactory: async (request: IRequestWithTenantConnection) => {
-    const tenantCode = request.headers['x-tenant-code'];
+  useFactory: async (request: any, tenantsService: TenantsService) => {
+    const tenantCode =
+      request.headers?.['x-tenant-code'] || request.data?.tenantCode;
 
     const connectionManager = getConnectionManager();
 
     if (tenantCode) {
-      const connectionName = request.tenantConnection.name;
+      const connectionName = tenantCode;
 
       if (connectionManager.has(connectionName)) {
         const connection = connectionManager.get(connectionName);
@@ -22,15 +24,17 @@ const connectionFactory: FactoryProvider = {
         );
       }
 
+      const tenant = await tenantsService.findOneByTenantCode(tenantCode);
+
       return createConnection({
         type: 'postgres',
         // TODO: Set the following properties in the Tenants Table
-        name: request.tenantConnection.name,
-        host: request.tenantConnection.host,
-        port: request.tenantConnection.port,
-        username: request.tenantConnection.username,
-        password: request.tenantConnection.password,
-        database: request.tenantConnection.database,
+        name: tenant.code,
+        host: tenant.host,
+        port: tenant.port,
+        username: tenant.username,
+        password: tenant.password,
+        database: tenant.database,
         entities: ['dist/modules/tenant-specific/**/*.entity{.ts,.js}'],
         migrations: ['dist/tenancy/migrations/tenant-specific/*{.ts,.js}'],
         migrationsRun: true,
@@ -40,12 +44,13 @@ const connectionFactory: FactoryProvider = {
       } as any);
     }
   },
-  inject: [REQUEST],
+  inject: [REQUEST, TenantsService],
 };
 
 @Global()
 @Module({
-  providers: [connectionFactory],
+  imports: [TenantsModule],
+  providers: [connectionFactory, TenantsService],
   exports: ['TENANT_CONNECTION'],
 })
 export class TenantConnectionModule {}
